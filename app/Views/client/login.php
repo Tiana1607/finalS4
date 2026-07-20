6c7a6a187b5fcc044a751b9bb231b9cad5e745f0
Tiana1607 <rovarajaonarimanana@gmail.com>
2026-07-20 10:22:25 +0300

    icon_v1

--- app/Views/client/login.php
+++ app/Views/client/login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Client</title>
-    <link rel="icon" href="<?= base_url('assets/bootstrap/icons/sim-fill.svg') ?>" style="color: red">
+    <link rel="icon" href="<?= base_url('assets/img/online-payment.png') ?>">
     <link rel="stylesheet" href="<?= base_url('assets/css/style.css') ?>">
 </head>
 <body>
