ee555d4380f2b9054220983a29bb85aa00bfa49a
Tiana1607 <rovarajaonarimanana@gmail.com>
2026-07-20 16:41:40 +0300

    a

--- app/Database/Seeds/TrancheMontantSeeder.php
+++ app/Database/Seeds/TrancheMontantSeeder.php
@@ -24,7 +24,7 @@ class TrancheMontantSeeder extends Seeder
         ];
 
         // ids: 2 = retrait, 3 = transfert (voir TypesOperationSeeder)
-        foreach ([1, 2, 3] as $typeOperationId) {
+        foreach ([2, 3] as $typeOperationId) {
             $data = [];
             foreach ($tranches as $t) {
                 $data[] = [
