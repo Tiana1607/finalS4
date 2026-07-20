8b7d4a21b63f24bc2cc27fb10ad0c6484fb4a321
Tiana1607 <rovarajaonarimanana@gmail.com>
2026-07-20 10:35:46 +0300

    js

--- app/Views/client/login.php
+++ app/Views/client/login.php
@@ -11,5 +11,5 @@
 <body>
     
 </body>
-script src="<?= base_url('assets/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+<script src="<?= base_url('assets/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
 </html>
