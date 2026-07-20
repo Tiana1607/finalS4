b137520ac2046c1254d0ade60a8b7db4c26f4085
Tiana1607 <rovarajaonarimanana@gmail.com>
2026-07-20 10:35:30 +0300

    js

--- app/Views/client/login.php
+++ app/Views/client/login.php
@@ -6,8 +6,10 @@
     <title>Login Client</title>
     <link rel="icon" href="<?= base_url('assets/img/online-payment.png') ?>">
     <link rel="stylesheet" href="<?= base_url('assets/css/style.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/bootstrap/css/bootstrap.min.css') ?>">
 </head>
 <body>
     
 </body>
+script src="<?= base_url('assets/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
 </html>
